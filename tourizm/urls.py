18b105730f115c9c115d0731e2a14f81f10a8bb3
unitedprogramming2024 <sohaib.radi@gmail.com>
2024-05-25 16:03:16 +0300

    new file:   build_files.sh 	modified:   tourizm/settings.py 	modified:   tourizm/urls.py 	new file:   vercel.json

--- tourizm/urls.py
+++ tourizm/urls.py
@@ -12,12 +12,12 @@ urlpatterns = [
     path('traveler/', include('traveler.urls')),
     path('destination/', include('Destination.urls')),
     path( "markers/", include("localisation.urls"),  ),
-   
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 admin.site.site_header = "TOURIZMO Admin"
 admin.site.site_title = "TOURIZMO Admin Portal"
 admin.site.index_title = "Welcome to TOURIZMO Trip Portal"
